d30351c58e1d1d95db5e2560e0a68e3ea1e54e45
Sai Teja <garlapati.saiteja1993@gmail.com>
2023-11-29 17:53:20 -0500

    Show Loading Skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,20 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card>
+      <Skeleton
+        height="200px"
+        width="300px"
+        borderRadius={10}
+        overflow={"hidden"}
+      >
+        <CardBody>
+          <SkeletonText></SkeletonText>
+        </CardBody>
+      </Skeleton>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
